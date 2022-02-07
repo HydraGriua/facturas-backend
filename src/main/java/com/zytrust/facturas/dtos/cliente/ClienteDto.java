9504f3a0bf456597143b4c2e43e42d572e4a1268
Flavio Saavedra <f.saavedra1810@gmail.com>
2022-02-07 11:38:45 -0500

    Agregando comentarios dentro de ClienteDto y CreateClienteDto

--- src/main/java/com/zytrust/facturas/dtos/cliente/ClienteDto.java
+++ src/main/java/com/zytrust/facturas/dtos/cliente/ClienteDto.java
@@ -1,23 +1,40 @@
+/*
+ * @(#)ClienteDto.java
+ *
+ * Copyright 2019 ZyTrust SA, Todos los derechos reservados.
+ * ZT PROPRIETARIO/CONFIDENTIALIDAD. Su uso está sujeto a los
+ * términos de la licencia adquirida a ZyTrust SA.
+ * No se permite modificar, copiar ni difundir sin autorización
+ * expresa de ZyTrust SA.
+ */
+
 package com.zytrust.facturas.dtos.cliente;
 
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
+/**
+ * Esta clase representa a un dto de cliente y debe ser usada para almacenar
+ * datos y mapearlos con una entidad cliente.
+ *
+ * @author Flavio Saavedra Montenegro
+ * @version 1, 07/02/2022
+ */
 
 @Getter
 @Setter
 public class ClienteDto {
 
-    private String clienteId;
-    private String numDocumento;
-    private String tipoDocumento;
-    private String primerNombre;
-    private String primerApellido;
-    private LocalDate fechaNacimiento;
-    private Character genero;
-    private String email;
-    private String celular;
-    private String direccion;
-    private String nombreEmpresa;
+    private String clienteId; /** Identificador de cliente */
+    private String numDocumento; /** Numero identificador de documento de cliente */
+    private String tipoDocumento; /** Tipo de documento de cliente */
+    private String primerNombre; /** Primer nombre de cliente */
+    private String primerApellido; /** Primer apellido de cliente */
+    private LocalDate fechaNacimiento; /** Fecha de nacimiento de cliente */
+    private Character genero; /** Genero de cliente */
+    private String email; /** Email de cliente */
+    private String celular; /** Celular de cliente */
+    private String direccion; /** Direccion de cliente */
+    private String nombreEmpresa; /** Nombre de empresa donde labora el cliente */
 }
--- src/main/java/com/zytrust/facturas/dtos/cliente/CreateClienteDto.java
+++ src/main/java/com/zytrust/facturas/dtos/cliente/CreateClienteDto.java
@@ -1,49 +1,75 @@
+/*
+ * @(#)CreateClienteDto.java
+ *
+ * Copyright 2019 ZyTrust SA, Todos los derechos reservados.
+ * ZT PROPRIETARIO/CONFIDENTIALIDAD. Su uso está sujeto a los
+ * términos de la licencia adquirida a ZyTrust SA.
+ * No se permite modificar, copiar ni difundir sin autorización
+ * expresa de ZyTrust SA.
+ */
+
 package com.zytrust.facturas.dtos.cliente;
 
+import java.time.LocalDate;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
+/**
+ * Esta clase representa a un dto de cliente y debe ser usada para almacenar
+ * datos y mapearlos con una entidad cliente.
+ *
+ * @author Flavio Saavedra Montenegro
+ * @version 1, 07/02/2022
+ */
 
 @Getter
 @Setter
 public class CreateClienteDto {
 
+    /** Numero identificador de documento de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String numDocumento;
 
+    /** Tipo de documento de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String tipoDocumento;
 
+    /** Primer nombre de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String primerNombre;
 
+    /** Primer apellido de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String primerApellido;
 
+    /** Fecha de nacimiento de cliente */
     private LocalDate fechaNacimiento;
 
+    /** Genero de cliente */
     private Character genero;
 
+    /** Email de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String email;
 
+    /** Celular de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String celular;
 
+    /** Direccion de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String direccion;
 
+    /** Nombre de empresa donde labora el cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String nombreEmpresa;
