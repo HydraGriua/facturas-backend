f616c04dfc07620242457960f407d5703b071995
Flavio Saavedra <f.saavedra1810@gmail.com>
2022-02-07 15:45:02 -0500

    Agregando comentarios dentro de FacturasApplication

--- src/main/java/com/zytrust/facturas/FacturasApplication.java
+++ src/main/java/com/zytrust/facturas/FacturasApplication.java
@@ -1,7 +1,24 @@
+/*
+ * @(#)FacturasApplication.java
+ *
+ * Copyright 2019 ZyTrust SA, Todos los derechos reservados.
+ * ZT PROPRIETARIO/CONFIDENTIALIDAD. Su uso está sujeto a los
+ * términos de la licencia adquirida a ZyTrust SA.
+ * No se permite modificar, copiar ni difundir sin autorización
+ * expresa de ZyTrust SA.
+ */
+
 package com.zytrust.facturas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Esta clase representa el componente principal de la aplicacion SpringBoot
+ *
+ * @author Flavio Saavedra Montenegro
+ * @version 1, 07/02/2022
+ */
 
 @SpringBootApplication
 public class FacturasApplication {
