8a632db2184d78f47ff1845ea11c2ea21b921f59
Flavio Saavedra <f.saavedra1810@gmail.com>
2022-02-07 11:55:36 -0500

    Agregando comentarios dentro de FacturaDto y CreateFacturaDto

--- src/main/java/com/zytrust/facturas/dtos/Factura/CreateFacturaDto.java
+++ src/main/java/com/zytrust/facturas/dtos/Factura/CreateFacturaDto.java
@@ -1,26 +1,38 @@
+/*
+ * @(#)CreateFacturaDto.java
+ *
+ * Copyright 2019 ZyTrust SA, Todos los derechos reservados.
+ * ZT PROPRIETARIO/CONFIDENTIALIDAD. Su uso está sujeto a los
+ * términos de la licencia adquirida a ZyTrust SA.
+ * No se permite modificar, copiar ni difundir sin autorización
+ * expresa de ZyTrust SA.
+ */
+
 package com.zytrust.facturas.dtos.Factura;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+/**
+ * Esta clase representa a un dto de creacion de factura y debe ser usada para almacenar
+ * datos y mapearlos con una entidad factura.
+ *
+ * @author Flavio Saavedra Montenegro
+ * @version 1, 07/02/2022
+ */
 
 @Getter
 @Setter
 public class CreateFacturaDto {
 
+    /** Direccion de emision de factura */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String direccion;
 
-    private LocalDateTime fechaEmision;
-
-    private BigDecimal subtotal;
-
+    /** Identificador de cliente */
     @NotEmpty
     @Size(min = 4, message = "Debe contener al menos 4 caracteres")
     private String clienteId;
--- src/main/java/com/zytrust/facturas/dtos/Factura/FacturaDto.java
+++ src/main/java/com/zytrust/facturas/dtos/Factura/FacturaDto.java
@@ -1,22 +1,40 @@
-package com.zytrust.facturas.dtos.Factura;
+/*
+ * @(#)FacturaDto.java
+ *
+ * Copyright 2019 ZyTrust SA, Todos los derechos reservados.
+ * ZT PROPRIETARIO/CONFIDENTIALIDAD. Su uso está sujeto a los
+ * términos de la licencia adquirida a ZyTrust SA.
+ * No se permite modificar, copiar ni difundir sin autorización
+ * expresa de ZyTrust SA.
+ */
 
-import lombok.Getter;
-import lombok.Setter;
+package com.zytrust.facturas.dtos.Factura;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Esta clase representa a un dto de factura y debe ser usada para almacenar
+ * datos y mapearlos con una entidad factura.
+ *
+ * @author Flavio Saavedra Montenegro
+ * @version 1, 07/02/2022
+ */
 
 @Getter
 @Setter
 public class FacturaDto {
 
-    private String facturaId;
-    private String direccion;
-    private LocalDateTime fechaEmision;
-    private LocalDateTime fechaPago;
-    private String tipoPago;
-    private BigDecimal subtotal;
-    private BigDecimal impuesto;
-    private BigDecimal total;
-    private String clienteId;
+    private String facturaId; /** Identificador de factura */
+    private String direccion; /** Direccion de emision de factura */
+    private LocalDateTime fechaEmision; /** Fecha y Hora de emision de factura */
+    private LocalDateTime fechaPago; /** Fecha y Hora de pago de factura */
+    private String tipoPago; /** Tipo de pago de factura */
+    private Character estado; /** Estado de factura */
+    private BigDecimal subtotal; /** subtotal a pagar de factura */
+    private BigDecimal impuesto; /** Impuesto a pagar de factura */
+    private BigDecimal total; /** Total a pagar de factura */
+    private String clienteId; /** Identificador de cliente */
 }
